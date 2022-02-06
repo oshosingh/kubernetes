bd40760eba33851d0b2de6494a88bc90dd3f323f
osho singh <oshosingh25@gmail.com>
2022-02-06 14:19:53 +0530

    create script file

---
+++ commands/minikube_install.sh
@@ -0,0 +1,14 @@
+sudo apt update -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update -y 
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+apt-get install -y conntrack
+minikube start --vm-driver=none
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
