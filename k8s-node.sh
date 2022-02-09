cc2f5a37eecb39546e39ac2c5261ebcb6be15855
osho singh <oshosingh25@gmail.com>
2022-02-09 15:03:45 +0530

    updated file

--- k8s-node.sh
+++ k8s-node.sh
@@ -1,4 +1,5 @@
 sudo apt update -y
+sudo apt-upgrade -y
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
