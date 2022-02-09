1a401ef3ccaa646baee71a61a79f67e271e7be11
osho singh <oshosingh25@gmail.com>
2022-02-09 16:12:14 +0530

    updated file

--- k8s-node.sh
+++ k8s-node.sh
@@ -1,12 +1,16 @@
 echo "================ Installing Docker ==========================="
 sudo apt update -y
-sudo apt-upgrade -y
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt update -y
+sudo apt update -y 
 apt-cache policy docker-ce
 sudo apt install docker-ce -y
+sleep 5
+
+echo "{\"exec-opts\": [\"native.cgroupdriver=systemd\"], \"storage-driver\": \"overlay2\" }" > /etc/docker/daemon.json
+systemctl daemon-reload
+systemctl restart docker
 
 echo "================= K8s setup started ==========================="
 
@@ -23,13 +27,6 @@ sudo apt-get update -y
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 
-echo "=================== Updating cGroupDrive ==========================="
-
-echo "{ "exec-opts": ["native.cgroupdriver=systemd"] }" > /etc/docker/daemon.json
-sudo systemctl daemon-reload
-sudo systemctl restart docker
-sudo systemctl restart kubelet
- 
 echo "==================Initializing Cluster networ ===================="
 
 kubeadm init --pod-network-cidr=10.244.0.0/16
