b7d7482dc9767970419bce3a63c168c4e10b76fd
osho singh <oshosingh25@gmail.com>
2022-02-10 10:40:01 +0530

    updated file

---
+++ k8s-install/worker-node.sh
@@ -0,0 +1,20 @@
+sudo apt update -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update -y 
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
+sleep 5
+
+echo "{\"exec-opts\": [\"native.cgroupdriver=systemd\"], \"storage-driver\": \"overlay2\" }" > /etc/docker/daemon.json
+systemctl daemon-reload
+systemctl restart docker
+
+sudo apt-get update -y
+sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update -y
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
