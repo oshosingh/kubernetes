6a93e7d5738c4c4328ffe96b90bc7d6ab75adf24
osho singh <oshosingh25@gmail.com>
2022-02-10 11:33:47 +0530

    updated file

---
+++ k8s-install/k8s-1node.sh
@@ -0,0 +1,37 @@
+echo "================ Installing Docker ==========================="
+sudo apt update -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update -y 
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
+sleep 5
+
+echo "{\"exec-opts\": [\"native.cgroupdriver=systemd\"], \"storage-driver\": \"overlay2\" }" > /etc/docker/daemon.json
+systemctl daemon-reload
+systemctl restart docker
+
+echo "================= K8s setup started ==========================="
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sudo sysctl --system
+sudo apt-get update -y
+sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update -y
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+echo "==================Initializing Cluster networ ===================="
+
+kubeadm init --pod-network-cidr=10.244.0.0/16
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl taint nodes --all node-role.kubernetes.io/master-
