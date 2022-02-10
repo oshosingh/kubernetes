6713fc0b197cc8349a5cce3e8b14cd448e39c1b1
osho singh <oshosingh25@gmail.com>
2022-02-10 11:37:26 +0530

    updated file

--- k8s-install/k8s-1node.sh
+++ k8s-install/k8s-1node.sh
@@ -35,3 +35,4 @@ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 kubectl taint nodes --all node-role.kubernetes.io/master-
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
