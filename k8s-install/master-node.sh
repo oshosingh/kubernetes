399f71bb655630613bb5f7b1b62b9b8e3d2bf954
osho singh <oshosingh25@gmail.com>
2022-02-16 11:22:31 +0530

    updated file

--- k8s-install/master-node.sh
+++ k8s-install/master-node.sh
@@ -34,5 +34,3 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-echo "================== Flannel Network interface ======================"
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
