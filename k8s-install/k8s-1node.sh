b4b1a83d1529c4c06fa96c99142b2caa119a02eb
osho singh <oshosingh25@gmail.com>
2022-02-16 14:04:18 +0530

    updated file

--- k8s-install/k8s-1node.sh
+++ k8s-install/k8s-1node.sh
@@ -36,3 +36,10 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 kubectl taint nodes --all node-role.kubernetes.io/master-
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+
+echo "===================Helm Install ==================================="
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+sudo apt-get install apt-transport-https --yes
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
