bdbd0d4665f7c7889cb912b26ccc84326e57f0e2
osho singh <oshosingh25@gmail.com>
2022-02-17 19:54:34 +0530

    updated file

---
+++ jupyterhub/nfserver.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+sudo apt install nfs-kernel-server -Y
+sudo mkdir /mnt/jupyterhub -p
+sudo chown nobody:nogroup /mnt/jupyterhub 
+sudo chmod 777 /mnt/jupyterhub 
+
+echo "/mnt/jupyterhub       *(rw,sync,no_subtree_check,no_root_squash,no_all_squash,insecure)" >> /etc/exports
+sudo exportfs -a #making the file share available
+
+sudo systemctl restart nfs-kernel-server #restarting the NFS kernel
