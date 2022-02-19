0d1020c499ca052af33f3bdbb09c4a162314843a
osho singh <oshosingh25@gmail.com>
2022-02-19 18:58:13 +0530

    updated file

--- jupyterhub/nfs/mount_nfs.sh
+++ jupyterhub/nfs/mount_nfs.sh
@@ -1,4 +1,4 @@
 sudo apt update
 sudo apt install nfs-common
 mkdir -p /root/mount
-sudo mount -t nfs 139.59.0.150:/mnt/jupyterhub /root/mount
+#sudo mount -t nfs 139.59.0.150:/mnt/jupyterhub /root/mount
